37170568be832114ae6114f5cbb0ee271e8e8339
Matias Marangone <matiasemarangone@gmail.com>
2021-04-25 12:59:08 -0300

    Test 7 failing, commit to continue later

--- _.js
+++ _.js
@@ -4,6 +4,19 @@ const _ = {
       let lowerClampedValue = Math.max(number,lower);
       let clampedValue = Math.min(lowerClampedValue,upper);
       return clampedValue;
+    },
+    inRange(number,start,end){
+      if(end === undefined){
+        end = start
+        start = 0
+      }
+      if(start > end){
+        let temp = end;
+        end = start;
+        start = temp;
+      }
+      let isInRange = start < number && number < end
+      return isInRange;
     }
   };
   
