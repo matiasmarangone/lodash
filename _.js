2522ecd20d5ec1b07478682a18f46ab30d978194
Matias Marangone <matiasemarangone@gmail.com>
2021-04-25 12:34:46 -0300

    Added clamp method, all test passed

--- _.js
+++ _.js
@@ -1,7 +1,14 @@
-const _ = {};
-
-
-
-
-// Do not write or modify code below this line.
-module.exports = _;
+const _ = {
+    clamp(number,lower,upper){
+      
+      let lowerClampedValue = Math.max(number,lower);
+      let clampedValue = Math.min(lowerClampedValue,upper);
+      return clampedValue;
+    }
+  };
+  
+  
+  
+  
+  // Do not write or modify code below this line.
+  module.exports = _;
